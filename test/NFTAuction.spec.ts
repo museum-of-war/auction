2edcae3b71dc6e732cabaf3082a1da4e08587e71
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-04-16 17:49:21 +0300

    add buy now and takeHighestBid tests

--- test/NFTAuction.spec.ts
+++ test/NFTAuction.spec.ts
@@ -75,4 +75,128 @@ describe("NFTAuction", function () {
         )
     ).to.be.revertedWith("Sender has no whitelisted NFTs");
   });
+
+  it("Should buy now and send fee", async function () {
+    const [signer, buyer, feeReceiver] = await ethers.getSigners();
+    const MockNFT = (await ethers.getContractFactory(
+      "MockNFT"
+    )) as MockNFT__factory;
+    const NFTAuction = (await ethers.getContractFactory(
+      "NFTAuction"
+    )) as NFTAuction__factory;
+
+    const whitelistedCollection = await MockNFT.deploy();
+    const auctionCollection = await MockNFT.deploy();
+
+    const auction = await NFTAuction.deploy([whitelistedCollection.address]);
+    await auction.deployed();
+
+    const auctionTokenId = 1;
+
+    await auctionCollection.mint(signer.address, auctionTokenId);
+
+    await auctionCollection.approve(auction.address, auctionTokenId);
+
+    const buyNowPrice = ethers.constants.WeiPerEther.mul(10);
+
+    await auction.createDefaultNftAuction(
+      auctionCollection.address,
+      auctionTokenId,
+      ethers.constants.AddressZero,
+      ethers.constants.WeiPerEther,
+      buyNowPrice,
+      [feeReceiver.address],
+      [10000] // 100.00%
+    );
+
+    const whitelistTokenId = 2;
+
+    await whitelistedCollection.mint(buyer.address, whitelistTokenId);
+
+    const balanceBefore = await feeReceiver.getBalance();
+
+    await auction
+      .connect(buyer)
+      .makeBid(
+        auctionCollection.address,
+        auctionTokenId,
+        ethers.constants.AddressZero,
+        0,
+        {
+          value: buyNowPrice,
+        }
+      );
+
+    expect(await auctionCollection.ownerOf(auctionTokenId)).to.be.equal(
+      buyer.address
+    );
+
+    const balanceAfter = await feeReceiver.getBalance();
+
+    expect(balanceAfter.sub(balanceBefore)).to.be.deep.equal(buyNowPrice);
+  });
+
+  it("Should create custom auction and end it", async function () {
+    const [signer, buyer, feeReceiver] = await ethers.getSigners();
+    const MockNFT = (await ethers.getContractFactory(
+      "MockNFT"
+    )) as MockNFT__factory;
+    const NFTAuction = (await ethers.getContractFactory(
+      "NFTAuction"
+    )) as NFTAuction__factory;
+
+    const whitelistedCollection = await MockNFT.deploy();
+    const auctionCollection = await MockNFT.deploy();
+
+    const auction = await NFTAuction.deploy([whitelistedCollection.address]);
+    await auction.deployed();
+
+    const auctionTokenId = 1;
+
+    await auctionCollection.mint(signer.address, auctionTokenId);
+
+    await auctionCollection.approve(auction.address, auctionTokenId);
+
+    await auction.createNewNftAuction(
+      auctionCollection.address,
+      auctionTokenId,
+      ethers.constants.AddressZero,
+      ethers.constants.WeiPerEther.div(100).mul(15),
+      ethers.constants.WeiPerEther.mul(10),
+      86400 * 5, // 5 days
+      5000, // 50.00%
+      [feeReceiver.address],
+      [10000] // 100.00%
+    );
+
+    const whitelistTokenId = 3;
+
+    await whitelistedCollection.mint(buyer.address, whitelistTokenId);
+
+    const balanceBefore = await feeReceiver.getBalance();
+
+    const bidPrice = ethers.constants.WeiPerEther.mul(2);
+
+    await auction
+      .connect(buyer)
+      .makeBid(
+        auctionCollection.address,
+        auctionTokenId,
+        ethers.constants.AddressZero,
+        0,
+        {
+          value: bidPrice,
+        }
+      );
+
+    await auction.takeHighestBid(auctionCollection.address, auctionTokenId);
+
+    expect(await auctionCollection.ownerOf(auctionTokenId)).to.be.equal(
+      buyer.address
+    );
+
+    const balanceAfter = await feeReceiver.getBalance();
+
+    expect(balanceAfter.sub(balanceBefore)).to.be.deep.equal(bidPrice);
+  });
 });
